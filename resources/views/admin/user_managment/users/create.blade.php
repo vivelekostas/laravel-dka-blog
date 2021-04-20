ee8559ff76777dfbe64eafe8615123190b317398
KonstantinGrebenyuk <vivelakostas1096@gmail.com>
2021-04-20 19:06:34 +0300

    Почти полный круд Юзера по дефолтной модели. Все необходимые шаблоны. Вывод списка юзеров в админке с возможностью создания, редактирования и удаления.

---
+++ resources/views/admin/user_managment/users/create.blade.php
@@ -0,0 +1,23 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+
+    <div class="container">
+        @component('admin.components.breadcrumb')
+            @slot('title') Создание новости @endslot
+            @slot('parent') Главная @endslot
+            @slot('active') Новости @endslot
+        @endcomponent
+
+        <hr>
+
+        <form class="form-horizontal" action="{{route('admin.user_managment.user.store')}}" method="post">
+            {{csrf_field()}}
+
+            {{-- Form include --}}
+            @include('admin.user_managment.users.partials.form')
+
+        </form>
+    </div>
+
+@endsection
